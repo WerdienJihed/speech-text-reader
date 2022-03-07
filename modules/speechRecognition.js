613963b76181051492cff0d97aedf46d792b114e
werdien jihed <informatique.jihed@gmail.com>
2022-03-07 16:27:48 +0100

    add edit mode functionality

--- modules/speechRecognition.js
+++ modules/speechRecognition.js
@@ -1,17 +1,29 @@
+/* Selecting Elements */
 const startListeningBtn = document.getElementById("startListeningBtn");
 const outputTextarea = document.getElementById("outputTextarea");
+const editMode = document.getElementById("editMode");
 
+/* Variables */
 const SpeechRecognition =
   window.speechRecognition || window.webkitSpeechRecognition;
+
 const recognition = new SpeechRecognition();
+
+/* Functions */
+const handleWriteResult = (e) => {
+  const result = e.results[0][0].transcript;
+  outputTextarea.value = result;
+};
 
 const handleStartListening = () => {
   recognition.start();
 };
 
-recognition.onresult = function (event) {
-  const result = event.results[0][0].transcript;
-  outputTextarea.value = result;
+const handleEditMode = (e) => {
+  outputTextarea.readOnly = !e.target.checked;
 };
 
+/* Event listeners */
+recognition.onresult = handleWriteResult;
 startListeningBtn.addEventListener("click", handleStartListening);
+editMode.addEventListener("change", handleEditMode);
