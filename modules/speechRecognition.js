64f5fe4bbb9c0fe4e5d83db9474eb55996a1f294
werdien jihed <informatique.jihed@gmail.com>
2022-03-07 16:34:25 +0100

    add copy to clipboard functionality

--- modules/speechRecognition.js
+++ modules/speechRecognition.js
@@ -2,13 +2,13 @@
 const startListeningBtn = document.getElementById("startListeningBtn");
 const outputTextarea = document.getElementById("outputTextarea");
 const editMode = document.getElementById("editMode");
+const copyBtn = document.getElementById("copyBtn");
 
 /* Variables */
 const SpeechRecognition =
   window.speechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition();
-
+const clipboard = navigator.clipboard;
 /* Functions */
 const handleWriteResult = (e) => {
   const result = e.results[0][0].transcript;
@@ -23,7 +23,21 @@ const handleEditMode = (e) => {
   outputTextarea.readOnly = !e.target.checked;
 };
 
+const changeCopyBtnIcon = () => {
+  copyBtn.firstElementChild.className = "bi bi-check-lg";
+  setTimeout(() => {
+    copyBtn.firstElementChild.className =
+      "bi bi-clipboard-fill text-secondary ";
+  }, 1000);
+};
+
+const handleCopy = () => {
+  changeCopyBtnIcon();
+  clipboard.writeText(outputTextarea.value);
+};
+
 /* Event listeners */
 recognition.onresult = handleWriteResult;
 startListeningBtn.addEventListener("click", handleStartListening);
 editMode.addEventListener("change", handleEditMode);
+copyBtn.addEventListener("click", handleCopy);
