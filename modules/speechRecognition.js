9becabe67c17e28a98f6198f87467af59fdcb5d7
werdien jihed <informatique.jihed@gmail.com>
2022-03-07 16:58:17 +0100

    add download as a text file functionality

--- modules/speechRecognition.js
+++ modules/speechRecognition.js
@@ -3,6 +3,7 @@ const startListeningBtn = document.getElementById("startListeningBtn");
 const outputTextarea = document.getElementById("outputTextarea");
 const editMode = document.getElementById("editMode");
 const copyBtn = document.getElementById("copyBtn");
+const downloadBtn = document.getElementById("downloadBtn");
 
 /* Variables */
 const SpeechRecognition =
@@ -36,8 +37,23 @@ const handleCopy = () => {
   clipboard.writeText(outputTextarea.value);
 };
 
+const handleDownload = () => {
+  const text = outputTextarea.value;
+  var blob = new Blob([text], { type: "text/plain" });
+  const href = URL.createObjectURL(blob);
+  const a = Object.assign(document.createElement("a"), {
+    href,
+    style: "display:none",
+    download: "test.txt",
+  });
+  a.click();
+  URL.revokeObjectURL(href);
+  a.remove();
+};
+
 /* Event listeners */
 recognition.onresult = handleWriteResult;
 startListeningBtn.addEventListener("click", handleStartListening);
 editMode.addEventListener("change", handleEditMode);
 copyBtn.addEventListener("click", handleCopy);
+downloadBtn.addEventListener("click", handleDownload);
